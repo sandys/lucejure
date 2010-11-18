72fd645935afbfe7f2fa9a36922d55b6b38e8de6
Sandeep Srinivasa <sandys@gmail.com>
2010-11-18 19:13:34 +0530

    made compatible with Lucene 3.0 API lots of api breakages

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject nihsearch "0.1.0-snapshot"
   :dependencies [[org.clojure/clojure "1.2.0"]
                   [org.clojure/clojure-contrib "1.2.0"]
-                  [org.apache.lucene/lucene-core "2.9.2"]
-                  [org.apache.lucene/lucene-highlighter "2.9.2"]]
+                  [org.apache.lucene/lucene-core "3.0.0"]
+                  ;[org.apache.lucene/lucene-core "2.9.2"]
+                  ;[org.apache.lucene/lucene-highlighter "2.9.2"]]
+                  [org.apache.lucene/lucene-highlighter "3.0.0"]]
   :dev-dependencies [[lein-run "1.0.0"]]
   :disable-implicit-clean true
   :namespaces :all
--- src/nihsearch.clj
+++ src/nihsearch.clj
@@ -12,8 +12,10 @@
            (org.apache.lucene.store FSDirectory)
            (org.apache.lucene.queryParser QueryParser$Operator
                                           MultiFieldQueryParser)
+           (org.apache.lucene.util Version)
            (java.io File File PushbackReader)
            (java.io.FileNotFoundException))
+  (:import [pitt.search.semanticvectors BuildIndex])
   (:use clojure.contrib.duck-streams
         clojure.contrib.str-utils
         clojure.contrib.seq-utils
@@ -38,16 +40,24 @@
 (defn index [^File file]
   (let [#^Document doc (Document.)
         pf (parse-file file)]
-    (.add doc (Field. "id" (:id pf) Field$Store/YES Field$Index/TOKENIZED ))
-    (.add doc (Field. "msg" (:msg pf) Field$Store/YES Field$Index/TOKENIZED  )) 
-    (with-open [iw ( doto (IndexWriter. (FSDirectory/getDirectory "/tmp/2") (StandardAnalyzer.) IndexWriter$MaxFieldLength/UNLIMITED)
-                          (.setRAMBufferSizeMB 20)
-                          (.setUseCompoundFile false))  ](.updateDocument iw (Term. "id" (:id pf)) doc) )))
+    (.add doc (Field. "id" (:id pf) Field$Store/YES Field$Index/ANALYZED))
+    (.add doc (Field. "msg" (:msg pf) Field$Store/YES Field$Index/ANALYZED)) 
+    ;(with-open [iw ( doto (IndexWriter. (FSDirectory/getDirectory "/tmp/2") (StandardAnalyzer.) IndexWriter$MaxFieldLength/UNLIMITED)
+    (with-open [iw ( doto (IndexWriter. (FSDirectory/open (File. "/tmp/2")) (StandardAnalyzer. Version/LUCENE_30) IndexWriter$MaxFieldLength/UNLIMITED)
+                          ;(.setRAMBufferSizeMB 20)
+                          ;(.setUseCompoundFile false)
+                          )  ](.updateDocument iw (Term. "id" (:id pf)) doc) )))
+
+(defn sem-index [^File file]
+  (BuildIndex/main (into-array String ["/tmp/2" ])))
 
 (defn -main [& args] 
   (alter-var-root #'*config* (fn [_] (read (PushbackReader. (reader "config.clj")))))
-  (IndexWriter/unlock (FSDirectory/getDirectory  "/tmp/2")) ;if not, then pre-existing "write.lock" files can cause lock-acquisition timeouts
+  ;(IndexWriter/unlock (FSDirectory/getDirectory  "/tmp/2")) ;if not, then pre-existing "write.lock" files can cause lock-acquisition timeouts
+  (IndexWriter/unlock (FSDirectory/open  (File. "/tmp/2"))) ;if not, then pre-existing "write.lock" files can cause lock-acquisition timeouts
   (let [{srcdir :srcdir} *config*]
       
            (do (println "sss " srcdir)
-               (map index (walk (File. "/home/user/Documents/articles/BMC_Clin_Pharmacol")) )) ) )
+               ;(map index (walk (File. "/home/user/Documents/articles/BMC_Clin_Pharmacol")) )
+               (sem-index (File. "/tmp/2") )
+               ) ) )
