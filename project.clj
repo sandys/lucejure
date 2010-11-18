80edfb36b58e306cae4ee356f80e265ecbecedc6
Sandeep Srinivasa <sandys@gmail.com>
2010-11-18 11:42:00 +0530

    adding lein-run dependency

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
                   [org.clojure/clojure-contrib "1.2.0"]
                   [org.apache.lucene/lucene-core "2.9.2"]
                   [org.apache.lucene/lucene-highlighter "2.9.2"]]
+  :dev-dependencies [[lein-run "1.0.0"]]
   :disable-implicit-clean true
   :namespaces :all
   :main nihsearch)
